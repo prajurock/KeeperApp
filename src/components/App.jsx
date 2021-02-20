8afa0d23d1c15ce8585fe379f6bcb38dffa1e7bc
prajurock <prajwalsingh651@gmail.com>
2021-02-20 13:08:24 +0530

    App updated

---
+++ src/components/App.jsx
@@ -0,0 +1,16 @@
+import Footer from "./Footer";
+import Header from "./Header";
+import Note from "./Note";
+import React from "react";
+
+function App() {
+    return (
+    <div>
+    <Header/>
+    <Note/>
+    <Footer/>
+    </div>
+    )
+};
+
+export default App;
