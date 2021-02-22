88d419cad4f6669626e33bd63d94e9066ce65d09
prajurock <prajwalsingh651@gmail.com>
2021-02-22 17:56:53 +0530

    added new functionality

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,23 +1,45 @@
+import React, { useState } from "react";
+
+import CreateArea from "./CreateArea";
 import Footer from "./Footer";
 import Header from "./Header";
 import Note from "./Note";
-import React from "react";
-import notes from "../note"
 
 function App() {
-    return (
+  const [notes, setNotes] = useState([]);
+
+  function addNote(newNote) {
+    setNotes(prevNotes => {
+      return [...prevNotes, newNote];
+    });
+  }
+
+  function deleteNote(id) {
+    setNotes(prevNotes => {
+      return prevNotes.filter((noteItem, index) => {
+        return index !== id;
+      });
+    });
+  }
+
+  return (
     <div>
-    <Header/>
-    {notes.map((x) =>
-         
-        <Note title={x.title}
-        content={x.content}
-            key={x.key}
-        />
-    )}
-    <Footer/>
+      <Header />
+      <CreateArea onAdd={addNote} />
+      {notes.map((noteItem, index) => {
+        return (
+          <Note
+            key={index}
+            id={index}
+            title={noteItem.title}
+            content={noteItem.content}
+            onDelete={deleteNote}
+          />
+        );
+      })}
+      <Footer />
     </div>
-    )
-};
+  );
+}
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -3,6 +3,3 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 ReactDOM.render(<App />, document.getElementById("root"));
-
-//Challenge. Render all the notes inside notes.js as a seperate Note
-//component.
